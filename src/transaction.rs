c0f3a2f841b00b399c87c4ea79c9f6db84930ea7
Carl Wallace <carl@redhoundsoftware.com>
2023-12-20 09:42:27 -0500

    add length check to get_version (#545)

--- src/transaction.rs
+++ src/transaction.rs
@@ -92,7 +92,7 @@ impl<'tx> Transaction<'tx> {
         // get version from device
         let response = Apdu::new(Ins::GetVersion).transmit(self, 261)?;
 
-        if !response.is_success() {
+        if !response.is_success() || response.data().is_empty() {
             return Err(Error::GenericError);
         }
 
